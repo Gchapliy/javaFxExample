ef101a9d07620f7f49ef8b85b647e4fd2c72ba58
Hryhorii <grishachapliy1@gmail.com>
2020-05-18 12:06:59 +0300

    init

---
+++ src/com/example/Main.java
@@ -0,0 +1,179 @@
+package com.example;
+
+import com.example.controller.PersonEditDialogController;
+import com.example.controller.PersonOverviewController;
+import com.example.model.Person;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.prefs.Preferences;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    public Main() {
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        // Устанавливаем иконку приложения.
+        this.primaryStage.getIcons().add(new Image("file:resources/images/icon.png"));
+        initRootLayout();
+
+        showPersonOverview();
+
+    }
+
+    private void initRootLayout() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+        try {
+            rootLayout = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Отображаем сцену, содержащую корневой макет.
+        Scene scene = new Scene(rootLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void showPersonOverview() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/PersonOverview.fxml"));
+        AnchorPane personOverview = null;
+        try {
+            personOverview = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Помещаем сведения об адресатах в центр корневого макета.
+        rootLayout.setCenter(personOverview);
+
+        // Даём контроллеру доступ к главному приложению.
+        PersonOverviewController controller = loader.getController();
+        controller.setMainApp(this);
+    }
+
+    /**
+     * Открывает диалоговое окно для изменения деталей указанного адресата.
+     * Если пользователь кликнул OK, то изменения сохраняются в предоставленном
+     * объекте адресата и возвращается значение true.
+     *
+     * @param person - объект адресата, который надо изменить
+     * @return true, если пользователь кликнул OK, в противном случае false.
+     */
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Загружаем fxml-файл и создаём новую сцену
+            // для всплывающего диалогового окна.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Создаём диалоговое окно Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Передаём адресата в контроллер.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+
+            // Отображаем диалоговое окно и ждём, пока пользователь его не закроет
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Возвращает главную сцену.
+     *
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    /**
+     * Возвращает preference файла адресатов, то есть, последний открытый файл.
+     * Этот preference считывается из реестра, специфичного для конкретной
+     * операционной системы. Если preference не был найден, то возвращается null.
+     *
+     * @return
+     */
+    public File getPersonFilePath() {
+        Preferences prefs = Preferences.userNodeForPackage(Main.class);
+        String filePath = prefs.get("filePath", null);
+        if (filePath != null) {
+            return new File(filePath);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Задаёт путь текущему загруженному файлу. Этот путь сохраняется
+     * в реестре, специфичном для конкретной операционной системы.
+     *
+     * @param file - файл или null, чтобы удалить путь
+     */
+    public void setPersonFilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(Main.class);
+        if (file != null) {
+            prefs.put("filePath", file.getPath());
+
+            // Обновление заглавия сцены.
+            primaryStage.setTitle("AddressApp - " + file.getName());
+        } else {
+            prefs.remove("filePath");
+
+            // Обновление заглавия сцены.
+            primaryStage.setTitle("AddressApp");
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
